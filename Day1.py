865daff0c37b9a2f4f5693af101efd7508ebea44
Varun Mohankumar <varunmj@users.noreply.github.com>
2023-05-21 16:06:12 -0500

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,8 @@
 print("Hello World!")  #Print function
+
+# https://app.codingrooms.com/w/hUiyyA0mSQ9f
+
+print("Day 1 - Python Print Function\n"+
+"The function is declared like this:\n"+
+"print('what to print')")
+
