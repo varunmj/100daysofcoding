34a016aa94524304e5f8c654925425880b48114b
Varun Mohankumar <varunmj@users.noreply.github.com>
2023-05-21 16:59:02 -0500

    Update Day1.py
    using input function

--- Day1.py
+++ Day1.py
@@ -15,3 +15,13 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+# e.g.
+
+# https://www.google.com/search?q=how+to+get+the+length+of+a+string+in+python+stack+overflow
+
+# Warning. Your program should work for different inputs. e.g. any name that you input
+
+print(len(input("What is your name? ")))
