b535fa95a21f46a863213e9f011eed7b20ff3e4c
Varun Mohankumar <varunmj@users.noreply.github.com>
2023-05-21 16:25:33 -0500

    Update Day1.py
    this is code 1.2

--- Day1.py
+++ Day1.py
@@ -6,3 +6,12 @@ print("Day 1 - Python Print Function\n"+
 "The function is declared like this:\n"+
 "print('what to print')")
 
+
+# I was asked to debug the code providede:
+
+# https://app.codingrooms.com/w/eu6lhgZvd5Ro
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
