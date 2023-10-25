0687c062b813019afaf3dbdf003939d5d482999c
Varun Mohankumar <varunmj@users.noreply.github.com>
2023-10-25 11:27:48 -0500

    day-2

--- Day2.py
+++ Day2.py
@@ -2,5 +2,16 @@
 #
 # print("hello"[4])
 
-num_char = len(input("whats your name: "))
-print("your name had " + str(num_char) +" characters")
+# num_char = len(input("whats your name: "))
+# print("your name had " + str(num_char) +" characters")
+
+
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+first = int(two_digit_number[0])
+second = int(two_digit_number[1])
+result =  first +second
+print(result)
